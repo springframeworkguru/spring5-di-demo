9fdc4a2c4c9cfd5947bf547b240880e681127284
John Thompson <john@springframework.guru>
2017-06-05 17:54:35 -0400

    adding bean lifecycle example

---
+++ src/main/java/guru/springframework/CustomBeanPostProcessor.java
@@ -0,0 +1,30 @@
+package guru.springframework;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 6/5/17.
+ */
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
